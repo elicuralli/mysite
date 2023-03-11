f5144bf23ea8004e92c0ad8a61316578c9431e19
elibeth curalli <elibethleonela@gmail.com>
2023-03-11 16:29:52 -0400

    first commit

---
+++ portfolio/scripts.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('elements.html')
+
+if __name__=='__main__':
+    app.run(debug = True)
+
+@app.route('/static/<path:path>')
+def static(path):
+    return send_from_directory('static', path)
